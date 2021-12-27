bde9a75c91bd66336da9df0ad06f5d48a9c89d99
Muizzer07 <muizxzer07@gmail.com>
2021-12-27 02:36:25 -0500

    added download functionlity

--- parsing.py
+++ parsing.py
@@ -2,13 +2,38 @@ import pandas as pd
 from bs4 import BeautifulSoup
 import requests
 
+cookies = {
+    'JSESSIONID': 'B6BE93A3B62F8F9CB8866C24DED59DF1',
+}
+
+headers = {
+    'Connection': 'keep-alive',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'Referer': 'http://www.gsea-msigdb.org/gsea/downloads.jsp',
+    'Accept-Language': 'en-US,en;q=0.9,bn;q=0.8,ar;q=0.7',
+}
+
+file_name = 'h.all.v7.4'
+
+requests.get('http://www.gsea-msigdb.org/gsea/msigdb/download_file.jsp', headers=headers, cookies=cookies, verify=False)
+symbol_res = requests.get(f'https://data.broadinstitute.org/gsea-msigdb/msigdb/release/7.4/{file_name}.symbols.gmt', headers=headers)
+entrez_res = requests.get(f'https://data.broadinstitute.org/gsea-msigdb/msigdb/release/7.4/{file_name}.entrez.gmt', headers=headers)
+
+with open(f'{file_name}.symbols.gmt', 'wb') as file:
+    file.write(symbol_res.content)
+
+with open(f'{file_name}.entrez.gmt', 'wb') as file:
+    file.write(entrez_res.content)
+
 lines_from_entrez = []
 lines_from_symbol = []
 
-with open('h.all.v7.4.entrez.gmt') as f:
+with open(f'{file_name}.entrez.gmt') as f:
     lines_from_entrez = f.readlines()
 
-with open('h.all.v7.4.symbols.gmt') as f:
+with open(f'{file_name}.symbols.gmt') as f:
     lines_from_symbol = f.readlines()
 
 main_parsed_df = pd.DataFrame([], columns=['ID', 'Entrez ID', 'Gene Name', 'Geneset Name', 'Geneset Link'])
