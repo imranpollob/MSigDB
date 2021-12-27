a205f9101703b01b6a980ad1017f6ae3b22ca5de
Muizzer07 <muizxzer07@gmail.com>
2021-12-27 02:18:04 -0500

    geneset info updated

--- data_model.py
+++ data_model.py
@@ -1,33 +1,40 @@
 import pandas as pd
 import sqlite3
+import os
 from os.path import exists
 
 db_file_name = 'msigdb.sqlite'
 file_exists = exists(db_file_name)
 
+if file_exists:
+    os.remove(db_file_name)
+
 parsed_file = pd.read_csv('parsed.csv')
 
 con = sqlite3.connect(db_file_name)
 cur = con.cursor()
-
-if not file_exists:
-    cur.execute('''CREATE TABLE gene_info
+    
+cur.execute('''CREATE TABLE gene_info
                 (entrez_id, gene_name)''')
 
-    cur.execute('''CREATE TABLE geneset_info
-                (name, collection_name, orgnism)''')
+cur.execute('''CREATE TABLE geneset_info
+            (name, collection_name, orgnism)''')
 
-    cur.execute('''CREATE TABLE associations
-                (gene_id, geneset_id)''')
+cur.execute('''CREATE TABLE associations
+            (gene_id, geneset_id)''')
+    
 
 for index in range(len(parsed_file)):
     query = 'INSERT INTO gene_info (entrez_id, gene_name) VALUES ("' + str(parsed_file['Entrez ID'][index]) + '", "'+ parsed_file['Gene Name'][index] + '");'
     cur.execute(query)
 
-    query = 'INSERT INTO geneset_info (name, collection_name, orgnism) VALUES ("' + str(parsed_file['Geneset Name'][index]) + '", "", "");'
-    cur.execute(query)
-    
     query = 'INSERT INTO associations (gene_id, geneset_id) VALUES ("' + str(parsed_file['Entrez ID'][index]) + '", "'+ parsed_file['Geneset Name'][index] + '");'
+    cur.execute(query)
+
+unique_genesets = parsed_file.drop_duplicates(subset=['Geneset Name'])
+
+for i, unique_geneset in unique_genesets.iterrows():
+    query = 'INSERT INTO geneset_info (name, collection_name, orgnism) VALUES ("' + str(unique_geneset['Geneset Name']) + '", "' + unique_geneset['Collection Name'] +'", "'+ unique_geneset['Organism'] +'");'
     cur.execute(query)
 
 con.commit()
--- parsing.py
+++ parsing.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from bs4 import BeautifulSoup
+import requests
 
 lines_from_entrez = []
 lines_from_symbol = []
@@ -23,13 +25,28 @@ for line, symbol_line in zip(lines_from_entrez, lines_from_symbol):
     geneset_entrez_ids = geneset_info[2:]
     gene_names_per_set = gene_names[2:]
     id_len = len(geneset_entrez_ids)
+    geneset_collection_name = ""
+    geneset_organism = ""
+
+
+    res = requests.get(geneset_link)
+    soup = BeautifulSoup(res.content, 'html.parser')
+    table_data = soup.find_all("table", {"class": "lists4"})[0]
+    
+    for table_row in table_data.find_all("tr"):
+        if table_row.find('th') and table_row.th.text == "Collection":
+            geneset_collection_name = table_row.td.text
+        if table_row.find('th') and table_row.th.text == "Organism":
+            geneset_organism = table_row.td.text
 
     parsed_df = pd.DataFrame({
         'ID': [*range(counter, counter+id_len)], 
         'Entrez ID': geneset_entrez_ids, 
         'Gene Name': gene_names_per_set,
         'Geneset Name': geneset_name,
-        'Geneset Link': geneset_link
+        'Geneset Link': geneset_link,
+        'Collection Name': geneset_collection_name,
+        'Organism': geneset_organism
     })
 
     main_parsed_df = main_parsed_df.append(parsed_df)
